e7c0af28ebc7ec0634a74f48f777a9dc0fbe7f14
Dave Nielsen <dnielsen@gmail.com>
2016-03-11 18:45:21 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,17 +8,17 @@ config.app = {
 };
 
 config.redis = {
-    port: process.env.REDIS_PORT || '14026',
-    host: process.env.REDIS_HOST || 'pub-redis-14026.us-east-1-4.6.ec2.redislabs.com',
-    auth: process.env.REDIS_AUTH || 'deepblue'
+    port: process.env.REDIS_PORT || '',
+    host: process.env.REDIS_HOST || '',
+    auth: process.env.REDIS_AUTH || ''
 };
 
 config.twitter =
 {
-    consumer_key: 'kCrZff4YLq0MtRNFNcYlwsJBL',
-    consumer_secret: 't5yqgsiwWertJyzcdEWi2hnSvbUKom69bPK0hCTx0kkpmBbZfk',
-    access_token_key: '12051112-NR65bNMMcmqgZL1Vf2f3myekR0wmxJDZ4JFGKiuoV',
-    access_token_secret: 'eKvrV80926rBpUq8sje2qz3k5GQaQbNN3j1xMB83HxpqJ'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 };
 
 module.exports = config;
